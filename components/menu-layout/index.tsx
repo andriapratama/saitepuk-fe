164ca9b6020cefd173afd8fcb909d7a3077a8d9a
Andria Pratama <andrilocong1st@gmail.com>
2022-07-04 12:25:43 +0800

    add state

--- components/menu-layout/index.tsx
+++ components/menu-layout/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { DrinkMenuContainer } from "../menu-container/drink-menu.container";
 import { FoodMenuContainer } from "../menu-container/food-menu.container";
+import { Modal } from "../modal";
 
 interface Data {
   id: number;
@@ -9,6 +10,7 @@ interface Data {
   category: string;
   subCategory: string;
   price: number;
+  image: string;
 }
 
 interface MenuLayout {
@@ -17,59 +19,129 @@ interface MenuLayout {
 }
 
 export const MenuLayout: React.FC<MenuLayout> = ({ menuValue, data }) => {
+  const [isShowModal, setIsShowModal] = useState<boolean>(false);
+  const [image, setImage] = useState<string>("");
+
   return (
     <>
+      <Modal
+        isShowModal={isShowModal}
+        setIsShowModal={setIsShowModal}
+        image={image}
+        setImage={setImage}
+      />
+
       {menuValue === "all" ? (
         <>
-          <FoodMenuContainer data={data} subCategory="main">
+          <FoodMenuContainer
+            data={data}
+            subCategory="main"
+            setIsShowModal={setIsShowModal}
+            setImage={setImage}
+          >
             Makanan
           </FoodMenuContainer>
 
-          <FoodMenuContainer data={data} subCategory="mie">
+          <FoodMenuContainer
+            data={data}
+            subCategory="mie"
+            setIsShowModal={setIsShowModal}
+            setImage={setImage}
+          >
             Mie
           </FoodMenuContainer>
 
-          <FoodMenuContainer data={data} subCategory="snack">
+          <FoodMenuContainer
+            data={data}
+            subCategory="snack"
+            setIsShowModal={setIsShowModal}
+            setImage={setImage}
+          >
             Snack
           </FoodMenuContainer>
 
-          <DrinkMenuContainer data={data} subCategory="hot">
+          <DrinkMenuContainer
+            data={data}
+            subCategory="hot"
+            setIsShowModal={setIsShowModal}
+            setImage={setImage}
+          >
             Minuman Hangat
           </DrinkMenuContainer>
 
-          <DrinkMenuContainer data={data} subCategory="ice">
+          <DrinkMenuContainer
+            data={data}
+            subCategory="ice"
+            setIsShowModal={setIsShowModal}
+            setImage={setImage}
+          >
             Minuman Dingin
           </DrinkMenuContainer>
 
-          <DrinkMenuContainer data={data} subCategory="bottle">
+          <DrinkMenuContainer
+            data={data}
+            subCategory="bottle"
+            setIsShowModal={setIsShowModal}
+            setImage={setImage}
+          >
             Minuman Botol
           </DrinkMenuContainer>
         </>
       ) : menuValue === "food" ? (
         <>
-          <FoodMenuContainer data={data} subCategory="main">
+          <FoodMenuContainer
+            data={data}
+            subCategory="main"
+            setIsShowModal={setIsShowModal}
+            setImage={setImage}
+          >
             Makanan
           </FoodMenuContainer>
 
-          <FoodMenuContainer data={data} subCategory="mie">
+          <FoodMenuContainer
+            data={data}
+            subCategory="mie"
+            setIsShowModal={setIsShowModal}
+            setImage={setImage}
+          >
             Mie
           </FoodMenuContainer>
 
-          <FoodMenuContainer data={data} subCategory="snack">
+          <FoodMenuContainer
+            data={data}
+            subCategory="snack"
+            setIsShowModal={setIsShowModal}
+            setImage={setImage}
+          >
             Snack
           </FoodMenuContainer>
         </>
       ) : menuValue === "drink" ? (
         <>
-          <DrinkMenuContainer data={data} subCategory="hot">
+          <DrinkMenuContainer
+            data={data}
+            subCategory="hot"
+            setIsShowModal={setIsShowModal}
+            setImage={setImage}
+          >
             Minuman Hangat
           </DrinkMenuContainer>
 
-          <DrinkMenuContainer data={data} subCategory="ice">
+          <DrinkMenuContainer
+            data={data}
+            subCategory="ice"
+            setIsShowModal={setIsShowModal}
+            setImage={setImage}
+          >
             Minuman Dingin
           </DrinkMenuContainer>
 
-          <DrinkMenuContainer data={data} subCategory="bottle">
+          <DrinkMenuContainer
+            data={data}
+            subCategory="bottle"
+            setIsShowModal={setIsShowModal}
+            setImage={setImage}
+          >
             Minuman Botol
           </DrinkMenuContainer>
         </>
