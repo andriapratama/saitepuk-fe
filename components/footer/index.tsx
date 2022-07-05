cd9711f10d088ab33542712af6ebd7b7f4ec4377
Andria Pratama <andrilocong1st@gmail.com>
2022-07-05 13:57:44 +0800

    add link wa and add gojek button

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import instagram from "../../icons/instagram.png";
 import whatsapp from "../../icons/whatsapp.png";
+import gojek from "../../icons/gojek.png";
 import cr from "../../icons/copyright-symbol.png";
 import { useRouter } from "next/router";
 
@@ -11,7 +12,7 @@ export const Footer = () => {
       <div className="w-full p-5">
         <div className="mb-5 flex w-full justify-center">
           <div
-            className="mx-2 flex h-[50px] w-[50px] items-center justify-center rounded-full bg-slate-200"
+            className="mx-1 flex h-[50px] w-[50px] items-center justify-center rounded-full bg-slate-200"
             onClick={() =>
               router.push("https://www.instagram.com/warung_saitepuk/")
             }
@@ -19,8 +20,22 @@ export const Footer = () => {
             <Image src={instagram} width="30px" height="30px" alt="instagram" />
           </div>
 
-          <div className="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-slate-200">
+          <div
+            className="mx-1 flex h-[50px] w-[50px] items-center justify-center rounded-full bg-slate-200"
+            onClick={() => router.push("https://wa.me/6281883412244")}
+          >
             <Image src={whatsapp} width="30px" height="30px" alt="whatsapp" />
+          </div>
+
+          <div
+            className="mx-1 flex h-[50px] w-[50px] items-center justify-center rounded-full bg-slate-200"
+            onClick={() =>
+              router.push(
+                "https://gofood.co.id/bali/restaurant/warung-sai-te-puuk-jln-gunung-merapi-sengguan-klungkung-dffe9fd0-bdea-4b76-9455-227649f86054"
+              )
+            }
+          >
+            <Image src={gojek} width="30px" height="30px" alt="gojek" />
           </div>
         </div>
         <div className="flex w-full justify-center">
