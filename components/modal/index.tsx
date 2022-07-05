799b7bc74e5561218bc4b7ce1ee536c69d04e9fb
Andria Pratama <andrilocong1st@gmail.com>
2022-07-05 11:49:58 +0800

    update

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -43,7 +43,7 @@ export const Modal: React.FC<Modal> = ({
 
       {isShowModal ? (
         <div
-          className="relative h-[50%] w-[90%] delay-200 xl:h-[90%] xl:w-[70%]"
+          className="relative h-[50%] w-[90%]"
           onClick={(e) => e.stopPropagation()}
         >
           <Image
@@ -51,6 +51,7 @@ export const Modal: React.FC<Modal> = ({
             alt="menu"
             layout="fill"
             objectFit="contain"
+            onLoad={(e) => console.log(e.target)}
           />
         </div>
       ) : null}
